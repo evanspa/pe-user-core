3e98f58de75033e0f5177bdae86ec7df499046e2
Paul Evans <evansp2@gmail.com>
2015-05-22 15:14:53 -0400

    Version 0.1.5-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pe-user-core "0.1.4"
+(defproject pe-user-core "0.1.5-SNAPSHOT"
   :description "A Clojure library encapsulating an abstraction modeling a user."
   :url "https://github.com/evanspa/pe-user-core"
   :license {:name "MIT"
